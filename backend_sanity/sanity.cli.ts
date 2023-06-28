03db83abb29dacca782961ddaa3b6dae595da8fc
Ferris Chang <103004499+PandaFChangs@users.noreply.github.com>
2023-06-27 23:31:42 -0400

    first commit

---
+++ backend_sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'cuth20j1',
+    dataset: 'production'
+  }
+})
---
+++ backend_sanity/schemas/index.ts
@@ -0,0 +1,10 @@
+import Abouts from './abouts'
+import Brands from './brands'
+import Contact from './contact'
+import Experiences from './experiences'
+import Skills from './skills'
+import Testimonials from './testimonials'
+import WorkExperience from './workExperience'
+import Works from './works'
+
+export const schemaTypes = [Abouts, Brands, Contact, Experiences, Skills, Testimonials, WorkExperience, Works]
