e4eae114ccdae18985145bc54cb5327e473de0b4
Ferris Chang <103004499+PandaFChangs@users.noreply.github.com>
2023-06-30 23:22:26 -0400

    corrected header styles

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -26,8 +26,8 @@ const Header = () => {
           <div className='badge-cmp app__flex'>
             <span>👋 </span>
             <div style={{ marginLeft: 20 }}>
-              <p className='p-text'>Hello, I am</p>
-              <h1 className='head-text'>Micael</h1>
+              <p className='p-text'>Hello, I'm</p>
+              <h1 className='head-text'>Ferris</h1>
             </div>
           </div>
           <div className='tag-cmp app__flex'>
@@ -36,6 +36,7 @@ const Header = () => {
           </div>
         </div>
       </motion.div>
+
       <motion.div
         whileInView={{ opacity: [0, 1] }}
         transition={{ duration: 0.5, delayChildren: 0.5 }}
@@ -58,7 +59,7 @@ const Header = () => {
       >
         {[images.flutter, images.redux, images.sass].map((circle, index) => (
           <div className='circle-cmp app__flex' key={`circle-${index}`}>
-            <img src={circle} alt='circle' /> 
+            <img src={circle} alt='profile_bg' /> 
           </div>
         ))}
       </motion.div>
