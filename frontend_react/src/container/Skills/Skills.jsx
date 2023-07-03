4d8aa40f527f6f05ccc5e0530dfb363ef58793f9
Ferris Chang <103004499+PandaFChangs@users.noreply.github.com>
2023-07-03 11:36:28 -0400

    skills css and jsx added

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
-import Tooltip from 'react-tooltip'
+import { Tooltip } from 'react-tooltip'
 
 import { AppWrap } from '../../wrapper'
 import { urlFor, client} from '../../client'
@@ -26,7 +26,7 @@ const Skills = () => {
       <h2 className='head-text'>Skills & Experience</h2>
       <div className='app__skills-container'>
         <motion.div className='app__skills-list'>
-          {skills.map((skill) => (
+          {skills?.map((skill) => (
             <motion.div
               whileInView={{opacity: [0, 1]}}
               transition={{duration: 0.5 }}
@@ -40,9 +40,47 @@ const Skills = () => {
             </motion.div>
           ))}
         </motion.div>
+        <motion.div className='app__skills-exp'>
+          {experiences?.map((experiences) => (
+              <motion.div 
+                className='app__skills-exp-item'
+                key={experiences.year}
+              >
+                <div className='app__skills-exp-year'>
+                  <p className='bold-text'>{experiences.year}</p>
+                </div>
+                <motion.div>
+                  {experiences.works.map((work) => (
+                    <>
+                      <motion.div
+                      whileInView={{opacity: [0, 1]}}
+                      transition={{duration: 0.5 }}
+                      className='app__skills-exp-work'
+                      data-tip
+                      data-for={work.name}
+                      key={work.name}
+                      >
+                        <h4 className='bold-text'>{work.name}</h4>
+                        <p className='p-text'>{work.company}</p>
+                      </motion.div>
+                      <Tooltip
+                      id={work.name}
+                      effect='solid'
+                      arrowColor='#fff'
+                      className='skills-tooltip'
+                      >
+                        {work.desc}
+                      </Tooltip>
+                    </>
+                  ))}
+                </motion.div>
+              </motion.div>
+
+          ))}
+        </motion.div>
       </div>
     </>
   )
 }
 
-export default Skills
+export default AppWrap(Skills, 'skills')
